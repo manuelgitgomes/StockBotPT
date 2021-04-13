9ea8d74ae2d37485e708e13bfb87b3b8bffe2907
Manuel <gomesnelito@gmail.com>
2021-04-13 20:30:54 +0100

    Write and read the csv.  Send several emails sim.

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from sender import *
 
 
 def main():
-    # Defines the constant emails and port
+    # Define the constant emails and port
     port= 465
     smtp_email = "smtp.gmail.com"
     sender_email = "checkstockPT@gmail.com"
-    #Defines the password and the receiver email
-    password, receiver_email = definer()
-    #Sends the email
-    sender(port, smtp_email, sender_email, receiver_email, password)
+    path = "receivers.csv"
+    #Define the password and the receiver emails
+    password = definer(path)
+    #Send the email
+    sender(port, smtp_email, sender_email, path, password)
+    
+
 
 
 
--- sender.py
+++ sender.py
@@ -5,60 +5,74 @@ from email.mime.multipart import MIMEMultipart
 import getpass
 import csv
 
-def definer():
-  # Defines the password and the receiver email
+def definer(path):
+  # Define the password
   password = getpass.getpass(prompt = 'Type your password and press enter: ')
-  receiver_email = input('Type the names and emails of the receivers: ')
-  return password, receiver_email
+  # Define the name and email of a new receiver, write that information on a csv file
+  with open(path, '+a') as f:
+    w = csv.writer(f, quoting=csv.QUOTE_ALL)
+    name = 'name'  
+    while (1):
+          name = input("Type the name of one of the new receivers: ")
+          if (not name):
+            break
+          email = input("And the corresponding email: ")
+          if (not email):
+            break
+          w.writerow([name, email])
 
-# Save the receivers on a csv
-# path = "receivers.csv"
-# with open(path, "wb", encoding='UTF8') as csv_file:
-#   writer = csv.writer(csv_file, delimiter=',')
-#   for line in receiver_email:
-#       writer.writerow(line)
+  return password
 
 
-def sender(port, smtp_email, sender_email, receiver_email, password):
-  # Create the message subject, from and to
-  message = MIMEMultipart("alternative")
-  message["Subject"] = "Stock Disponível!"
-  message["From"] = sender_email
-  message["To"] = receiver_email
 
-  # Create the plain-text and HTML version of your message
-  text = """\
-  Olá!
-  Somos a Check Stock PT e estamos a testar o envio de emails para detetar stocks.
-  Em breve irás receber atualizações de stock das tuas GPUs favoritas!
-  Até à próxima!"""
-  html = """\
-  <html>
-  <body>
-    <p>Olá!<br>
-      Somos a Check Stock PT e estamos a testar o envio de emails para detetar stocks.<br>
-      Em breve irás receber atualizações de stock das tuas GPUs favoritas!<br>
-      Até à próxima!
-    </p>
-  </body>
-  </html>
-  """
 
-  # Turn these into plain/html MIMEText objects
-  part1 = MIMEText(text, "plain")
-  part2 = MIMEText(html, "html")
+def sender(port, smtp_email, sender_email, path, password):
+  # Read the csv file and sends the email for each receiver
+  with open(path) as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip header row
+    for name, email in reader:
+        print("Sending email to " + name)
 
-  # Add HTML/plain-text parts to MIMEMultipart message
-  # The email client will try to render the last part first
-  message.attach(part1)
-  message.attach(part2)
+        # Create the message subject, from and to
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Stock Disponível!"
+        message["From"] = sender_email
+        message["To"] = email
 
-  # Create secure connection with server and send email
-  context = ssl.create_default_context()
-  with smtplib.SMTP_SSL(smtp_email, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, receiver_email, message.as_string()
-    )
+        # Create the plain-text and HTML version of your message
+        text = """\
+        Olá, """ + name + """!
+        Somos a Check Stock PT e estamos a testar o envio de emails para detetar stocks.
+        Em breve irás receber atualizações de stock das tuas GPUs favoritas!
+        Até à próxima!"""
+        html = """\
+        <html>
+        <body>
+          <p>Olá, """ + name + """!<br>
+            Somos a Check Stock PT e estamos a testar o envio de emails para detetar stocks.<br>
+            Em breve irás receber atualizações de stock das tuas GPUs favoritas!<br>
+            Até à próxima!
+          </p>
+        </body>
+        </html>
+        """
+
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
+        message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_email, port, context=context) as server:
+          server.login(sender_email, password)
+          server.sendmail(
+              sender_email, email, message.as_string()
+          )
 
 
