2fa6bd7acc473581ee178c5b289595390b682392
Manuel <gomesnelito@gmail.com>
2021-04-22 14:30:35 +0100

    After an email is sent, continue scanning. Better comments

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from scanner import *
 
 
 def main():
-    # Define the constant emails and port
+    # Define the constant emails, port, paths and a counter
     port= 465
     smtp_email = "smtp.gmail.com"
     sender_email = "checkstockPT@gmail.com"
     Rpath = "receivers.csv"
     Wpath = "websites.csv"
+    c = 0
     # Define the password and the receiver emails
     password = definer(Rpath)
-    # Scan webpages and return gpu available and correpondent website
-    gpu, chip, site, store, price = scanner(Wpath)
-    # Send the email
-    sender(port, smtp_email, sender_email, Rpath, password, gpu, site, price)
-    
+
+    # After an email is sent, continue scanning
+    while (1):
+        # Scan webpages and return gpu available and correpondent website
+        gpu, chip, site, store, pricestr, priceint, c = scanner(Wpath, c)
+        # Send the email
+        sender(port, smtp_email, sender_email, Rpath, password, gpu, site, pricestr, priceint)
 
 
 
